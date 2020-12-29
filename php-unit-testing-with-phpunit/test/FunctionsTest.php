5e02ea458b3946041de34e9cef177b7d3fc7993f
Ken <kencho.gigascience@gmail.com>
2020-12-29 16:26:52 +0800

    Add not equal assertion

--- php-unit-testing-with-phpunit/test/FunctionsTest.php
+++ php-unit-testing-with-phpunit/test/FunctionsTest.php
@@ -10,6 +10,10 @@ class FunctionsTest extends TestCase
 
         $this->assertEquals(4, add(2, 2));
         $this->assertEquals(8, add(3, 5));
+    }
 
+    public function testAddDoesNotReturnTheIncorrectSum()
+    {
+        $this->assertNotEquals(5, add(2, 2));
     }
 }
