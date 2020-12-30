55c3348d50658978b1497ffa0938ff8af593350b
Ken <kencho.gigascience@gmail.com>
2020-12-30 18:01:12 +0800

    Add queue files

---
+++ php-unit-testing-with-phpunit/src/queue.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class queue
+{
+    /**
+     * Queue items
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * Add item to the end of the queue
+     * @param $item
+     */
+    public function push($item)
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * Take an item from the head of the queue
+     * @return mixed
+     */
+    public function pop()
+    {
+        return array_pop($this->items);
+    }
+
+    /**
+     * Count the total number of the queue
+     * @return int
+     */
+    public function getCount()
+    {
+        return count($this->items);
+    }
+}
