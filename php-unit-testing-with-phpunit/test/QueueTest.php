ddae8bd3bc26c2e56afcd72239562dd45ebb95e2
Ken <kencho.gigascience@gmail.com>
2020-12-30 18:06:25 +0800

    Test push and pop functions

--- php-unit-testing-with-phpunit/test/QueueTest.php
+++ php-unit-testing-with-phpunit/test/QueueTest.php
@@ -9,4 +9,21 @@ class QueueTest extends TestCase
         $queue = new Queue;
         $this->assertEquals(0, $queue->getCount());
     }
+
+    public function testAnItemIsAddedToTheQueue()
+    {
+        $queue = new Queue;
+        $queue->push('green');
+        $this->assertEquals(1, $queue->getCount());
+    }
+
+    public function testAnItemIsRemovedFromTheQueue()
+    {
+        $queue = new Queue;
+        $queue->push('green');
+        $item = $queue->pop();
+
+        $this->assertEquals(0, $queue->getCount());
+        $this->assertEquals('green', $item);
+    }
 }
