57c0d364f4787e2d76da72bf89f7caabc8c83d66
Ken <kencho.gigascience@gmail.com>
2021-01-02 23:36:53 +0800

    Test the mock mailer object

--- php-unit-testing-with-phpunit/test/UserTest.php
+++ php-unit-testing-with-phpunit/test/UserTest.php
@@ -29,4 +29,28 @@ class UserTest extends TestCase
         $user->first_name = "Peter";
         $this->assertEquals('Peter', $user->first_name);
     }
+
+    public function testNotificationIsSent()
+    {
+        $user = new User;
+
+        /**
+         * To create mock mailer class to test
+         * but it will not work, as mailer object is hardcoded in the user class
+         */
+        $mock_mailer = $this->createMock(Mailer::class);
+        $mock_mailer->method('sendMessage')->willReturn(true);
+
+        /**
+         * Here shows how do we tell the User class to use the mock?
+         * Dependency Injection
+         */
+
+//        $user->setMailer(new Mailer); //Real Mailer object
+        $user->setMailer($mock_mailer);//Pass in the mock Mailer object
+
+        $user->email = 'dave@example.com';
+        $this->assertTrue($user->notify("Hello")); //this will delay for 3s.
+    }
+
 }
