0dea22fb890b4172d617990a607e2355d5e084e6
Ken <kencho.gigascience@gmail.com>
2021-01-04 12:23:26 +0800

    Add mockery test

--- php-unit-testing-with-phpunit/test/WeatherMonitorTest.php
+++ php-unit-testing-with-phpunit/test/WeatherMonitorTest.php
@@ -38,4 +38,15 @@ class WeatherMonitorTest extends TestCase
         $this->assertEquals(23, $weather->getAverageTemperature('12:00', '14:00'));
 
     }
+
+    public function testCorrectAverageIsReturnedWithMockery()
+    {
+        $service = Mockery::mock(TemperatureService::class);
+
+        $service->shouldReceive('getTemperature')->once()->with('12:00')->andReturn(20);
+        $service->shouldReceive('getTemperature')->once()->with('14:00')->andReturn(26);
+
+        $weather = new WeatherMonitor($service);
+        $this->assertEquals(23, $weather->getAverageTemperature('12:00', '14:00'));
+    }
 }
