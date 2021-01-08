d02013befbdcaa0636ff221ca145ae545c0539dc
choling <cholingken@gmail.com>
2021-01-08 16:09:45 +0800

    Create getSlug method

--- phpunitCourse/src/App/Article.php
+++ phpunitCourse/src/App/Article.php
@@ -2,7 +2,16 @@
 
 namespace App;
 
+use PhpParser\Builder\Function_;
+use SebastianBergmann\CodeUnit\FunctionUnit;
+
 class Article 
 {
     public $title;
+
+    public Function getSlug()
+    {
+        //return empty string
+        return "";
+    }
 }
--- phpunitCourse/tests/ArticleTest.php
+++ phpunitCourse/tests/ArticleTest.php
@@ -2,12 +2,24 @@
 
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
+
 class ArticleTest extends TestCase
 {
     public function testTitleIsEmptyByDefault()
     {
         $article = new App\Article;
-
         $this->assertEmpty($article->title);
+    }
+
+    public function testSlugIsEmptyWithNoTitle()
+    {
+        $article = new App\Article;
+        
+        //assertEquals just does a boolean comparsion, so null and an empty string are equal
+        // $this-assertEquals($article->getSlug(), "");
+
+        //assertSame asserts the values are actually identicle
+        $this->assertSame($article->getSlug(), "");
     }
 }
