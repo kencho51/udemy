c782b8be50ffca911a0aa99c0d84af7a30bb9f09
Ken <kencho.gigascience@gmail.com>
2021-01-11 12:34:59 +0800

    To pass test on protect and private method

---
+++ phpunitCourse/src/ItemChild.php
@@ -0,0 +1,10 @@
+<?php
+
+class ItemChild extends Item
+{
+    public function getID()
+    {
+       return parent::getID();
+    }
+
+}
--- phpunitCourse/tests/ItemTest.php
+++ phpunitCourse/tests/ItemTest.php
@@ -9,4 +9,29 @@ class ItemTest extends TestCase
         $item = new Item;
         $this->assertNotEmpty($item->getDescription());
     }
+
+    public function testIDisAnInteger()
+    {
+        $item = new ItemChild();
+        $this->assertIsInt($item->getID());
+    }
+
+    //Usually dont test private method, or have to use reflection
+    public function testTokenIsAString()
+    {
+        $item = new Item();
+
+        //Create a reflector object
+        $reflector = new ReflectionClass(Item::class);
+        $method = $reflector->getMethod('getToken');
+
+        //Make the private method accessible
+        $method->setAccessible(true);
+
+        //Pass in object that we are reflecting
+        //Item object will be created at the top of the test method
+        $result = $method->invoke($item);
+
+        $this->assertIsString($result);
+    }
 }
