57734e8f35e399500bcf3c7b29376ac44a2acd5d
Ken <kencho.gigascience@gmail.com>
2021-01-11 13:16:48 +0800

    Create mock method to test abstract class

--- phpunitCourse/tests/AbstractPersonTest.php
+++ phpunitCourse/tests/AbstractPersonTest.php
@@ -4,11 +4,26 @@ use PHPUnit\Framework\TestCase;
 
 class AbstractPersonTest extends TestCase
 {
+    //Create child class of the abstract class and run test on that
     public function testNameAndTitleIsReturned()
     {
-        //Create child class of the abstract class and run test on that
         $doctor = New Doctor('Green');
 
         $this->assertEquals('Dr. Green', $doctor->getNameAndTitle());
     }
+
+    //Alternatively, use getMockBuilder method
+    public function testNameAndTitleIncludesValueFromGetTitle()
+    {
+        $mock = $this->getMockBuilder(AbstractPerson::class)
+                    ->setConstructorArgs(['Green'])
+                    ->getMockForAbstractClass();
+
+        //Mock the abstract method and return the default value
+        $mock->method('getTitle')
+            ->willReturn('Dr.');
+
+        $this->assertEquals('Dr. Green', $mock->getNameAndTitle());
+    }
+
 }
