e4eaac03bba6fb38b5753b244cf8181515cd9e20
Ken <kencho.gigascience@gmail.com>
2021-01-15 17:46:15 +0800

    Create destructor

--- oop-php-tdd-phpunit-from-scratch/oop/index.php
+++ oop-php-tdd-phpunit-from-scratch/oop/index.php
@@ -25,10 +25,17 @@ class Video
     {
         return $this->published ? "This video is paused." : "";
     }
+
+    public function __destruct()
+    {
+        var_dump('Destroying instance of '. get_class());
+    }
 }
 
 //header('Content-Type:text/plain', true);
 $introduction = new Video('mp4', '10.30', 'Introduction to OOP');
 
-$video2 = new Video(); //will throw error
+
+//$video2 = new Video(); //will throw error
+$video2 = new Video('mp4', '12.30', 'The second video');
 var_dump($video2);
