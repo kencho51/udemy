e103c7809c0a8f3802ce8d3962af11adfd8891f1
Ken <kencho.gigascience@gmail.com>
2021-01-18 15:34:06 +0800

    Use different namespace

--- oop-php-tdd-phpunit-from-scratch/src/Student.php
+++ oop-php-tdd-phpunit-from-scratch/src/Student.php
@@ -3,11 +3,18 @@
 namespace App;
 
 use Acme\Test;
+use PDOException;
+use PDO;
 
 class Student
 {
     public function __construct(Record $record, Test $test)
     {
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=store', 'user', 'password');
+        }catch (PDOException $ex){
+            var_dump($ex->getMessage());
+        }
         var_dump("from inside namespace");
     }
 }
