8717feb7f7d7d6ebcd21e54e4af16f4830ce6e7c
Ken <kencho.gigascience@gmail.com>
2021-01-18 15:34:24 +0800

    Namespace use cases

--- oop-php-tdd-phpunit-from-scratch/callStudent.php
+++ oop-php-tdd-phpunit-from-scratch/callStudent.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Student;
+use App\Record;
+use Acme\Test;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 header('Content-Type:text/plain', true);
 
-new Student();
+new Student(new Record(), new Test());
