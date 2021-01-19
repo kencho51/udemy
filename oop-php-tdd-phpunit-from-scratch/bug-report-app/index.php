7f20142e5cef0eec0c3a2f0f9349f5a35b5dbd35
Ken <kencho.gigascience@gmail.com>
2021-01-19 10:35:32 +0800

    Set up env

--- oop-php-tdd-phpunit-from-scratch/bug-report-app/index.php
+++ oop-php-tdd-phpunit-from-scratch/bug-report-app/index.php
@@ -4,5 +4,9 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$config = \App\helpers\Config::get('app', 'app_name');
-var_dump($config);
+$application = new \App\helpers\App();
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+echo $application->isRunningFromConsole() . PHP_EOL;
