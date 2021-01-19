5e7cd4807c93b840ce820fe563152840244fbbc0
Ken <kencho.gigascience@gmail.com>
2021-01-19 10:38:42 +0800

    Show command line msg

--- oop-php-tdd-phpunit-from-scratch/bug-report-app/index.php
+++ oop-php-tdd-phpunit-from-scratch/bug-report-app/index.php
@@ -9,4 +9,10 @@ echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
 echo $application->getLogPath() . PHP_EOL;
 echo $application->getEnvironment() . PHP_EOL;
 echo $application->isDebugMode() . PHP_EOL;
-echo $application->isRunningFromConsole() . PHP_EOL;
+//echo $application->isRunningFromConsole() . PHP_EOL;
+if ($application->isRunningFromConsole()) {
+    echo 'Form command line';
+} else {
+    echo 'From browser';
+}
+
